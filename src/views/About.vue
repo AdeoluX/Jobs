cdcc8b7a1785e751593128d05e0f6a9dd191e847
TAdejuwon <tadejuwon@chapelhilldenham.com>
2022-02-28 15:58:10 +0100

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto, velit ea
+      eius voluptatibus suscipit quaerat voluptates porro accusantium doloribus
+      sequi ullam veritatis explicabo id aut. Ducimus doloremque voluptatem
+      quisquam. Asperiores temporibus recusandae, rerum quia consequatur
+      assumenda labore suscipit corporis magni, neque quis, eveniet nihil maxime
+      tempora quos iste ad ipsam. Rem dolore reiciendis cumque quod, molestiae
+      expedita. Nulla voluptates consequatur nostrum deleniti, veniam magni,
+      corporis harum atque iure perspiciatis cupiditate. Ea nostrum veniam vel
+      molestias, tenetur culpa sed sapiente atque qui quos maiores quibusdam
+      distinctio ut eos suscipit molestiae, ipsam nemo, soluta architecto.
+      Ratione dolor laudantium esse reprehenderit optio error.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto, velit ea
+      eius voluptatibus suscipit quaerat voluptates porro accusantium doloribus
+      sequi ullam veritatis explicabo id aut. Ducimus doloremque voluptatem
+      quisquam. Asperiores temporibus recusandae, rerum quia consequatur
+      assumenda labore suscipit corporis magni, neque quis, eveniet nihil maxime
+      tempora quos iste ad ipsam. Rem dolore reiciendis cumque quod, molestiae
+      expedita. Nulla voluptates consequatur nostrum deleniti, veniam magni,
+      corporis harum atque iure perspiciatis cupiditate. Ea nostrum veniam vel
+      molestias, tenetur culpa sed sapiente atque qui quos maiores quibusdam
+      distinctio ut eos suscipit molestiae, ipsam nemo, soluta architecto.
+      Ratione dolor laudantium esse reprehenderit optio error.
+    </p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis
+      quidem quae accusamus eaque culpa! Tenetur officiis a cumque error soluta
+      amet exercitationem sed fuga dicta maiores. Provident beatae deserunt
+      assumenda tempore excepturi vero neque ad enim minima consectetur
+      inventore eveniet vel nulla, eaque explicabo eius eum quod asperiores
+      vitae. Autem atque enim minima! Similique, aut ut aspernatur velit modi
+      exercitationem ad reiciendis, doloremque sequi nam culpa atque
+      perspiciatis voluptates quasi corporis ullam eos! Et, fuga saepe obcaecati
+      ab exercitationem nobis. Sequi quaerat quo voluptatibus, ad consectetur
+      cupiditate quis eligendi consequatur esse eaque maiores doloribus
+      excepturi! Aspernatur ipsa aperiam rerum officiis?
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis
+      quidem quae accusamus eaque culpa! Tenetur officiis a cumque error soluta
+      amet exercitationem sed fuga dicta maiores. Provident beatae deserunt
+      assumenda tempore excepturi vero neque ad enim minima consectetur
+      inventore eveniet vel nulla, eaque explicabo eius eum quod asperiores
+      vitae. Autem atque enim minima! Similique, aut ut aspernatur velit modi
+      exercitationem ad reiciendis, doloremque sequi nam culpa atque
+      perspiciatis voluptates quasi corporis ullam eos! Et, fuga saepe obcaecati
+      ab exercitationem nobis. Sequi quaerat quo voluptatibus, ad consectetur
+      cupiditate quis eligendi consequatur esse eaque maiores doloribus
+      excepturi! Aspernatur ipsa aperiam rerum officiis?
+    </p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  components: {},
+};
 </script>
